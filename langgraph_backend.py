96ec0c95c9849269fe72d06a5ba1a453bf7ecd04
zeel <zeelsonani7@gmail.com>
2026-09-01 17:51:55 +0530

    name assign

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -2,7 +2,7 @@ from langgraph.graph import StateGraph,START,END
 from langchain_groq import ChatGroq
 from typing import TypedDict, Annotated
 from dotenv import load_dotenv
-from langchain_core.messages import BaseMessage,HumanMessage
+from langchain_core.messages import BaseMessage,HumanMessage,SystemMessage
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import MemorySaver
 
@@ -11,7 +11,7 @@ load_dotenv()
 model = ChatGroq(model='openai/gpt-oss-120b')
 
 class ChatState(TypedDict):
-    messages : Annotated[list[BaseMessage],add_messages]
+    messages : Annotated[list[BaseMessage],add_messages] = [SystemMessage('you are Quasari if user ask who are you and what is your name so you have to response Quasari not ChatGPT or else , basically replace chatgpt with Quasari.')]
 
 def chat(state:ChatState):
     prompt = state['messages']
